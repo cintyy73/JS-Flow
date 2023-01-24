ce681079b6486fd1d93d7079074a38b602d7907d
cintyy <cintiapaezdm@gmail.com>
2023-01-24 13:40:25 -0300

    Creo función renderJobs

---
+++ app.js
@@ -0,0 +1,44 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+const BASE_URL = "https://63cfafea8a780ae6e67a7e98.mockapi.io/";
+
+const renderJobs = async () => {
+    try {
+        const response = await fetch(`${BASE_URL}jobs`);
+        const jobs = await response.json();
+        $("#cont-cards").innerHTML = '';
+        for (const {name, description, location, seniority, category} of jobs) {
+            $("#cont-cards").innerHTML += `    
+            <div class="card column is-3 m-2 p-3 ">
+                <div class="content">
+                    <div class="media">
+                        <p id="name" class="subtitle is-5">${name}</p>
+                    </div>
+                </div>
+                <div class="content">
+                    <p id="description" class="is-size-7">${description}</p>
+                </div>
+                <div id="tags" class="media">
+                    <p class="has-text-dark is-size-7 has-background-primary p-1 m-1" id="location">${location}</p>
+                    <p class="has-text-dark is-size-7 has-background-primary p-1 m-1" id="seniority">${seniority}</p>
+                    <p class="has-text-dark is-size-7 has-background-primary p-1 m-1" id="category">${category}</p>
+                </div>
+                <div class="control">
+                    <button id="btn-details" class="button is-small is-link">
+                        See details
+                    </button>
+                </div>
+            </div>`
+        }
+
+    } catch (error) {
+        $("#cont-cards").innerHTML = `
+        <article class="message is-danger">
+            <div class="message-body ">
+                <p>Pagina no disponible, vuelva intentar en unos minutos.</p>
+            </div>
+        </article>`;
+    }
+}
+renderJobs()
